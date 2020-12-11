d04a6c6b82a522ebb4521caea7f1e56243b744b8
pricemou <scameleons3@gmail.com>
2020-12-11 10:55:20 +0100

    button

--- models/classe.py
+++ models/classe.py
@@ -1,7 +1,11 @@
 from odoo import models, fields, api
+import logging
+
+_logger  = logging.getLogger(__name__)
 
 class Ufec_classe(models.Model):
     _name = 'ufec.classe'
+    _inherit = 'mail.thread'
     _rec_name='classe_nom'
 
     classe_nom = fields.Char(string='Nom')
@@ -17,4 +21,23 @@ class Ufec_classe(models.Model):
                             relation='class_subj_ref',
                             column1='classe_nom',
                             column2='nom')
+
+    num_prof = fields.Integer(string='Nombre de Professeurs', compute='comp_prof')
+    num_sub = fields.Integer(string='Nombre des Matières', compute='comp_sub')
+    num_ets = fields.Integer(string='Nombre des Etudiants', compute='comp_stu')
     
+    # champ calculer
+    def comp_prof(self):
+        self.num_prof = len(self.professeurs_ids)
+
+    def comp_sub(self):
+        self.num_sub = len(self.subject_ids)
+    
+    def comp_stu(self):
+        self.num_ets = len(self.student_ids)
+            
+    
+    @api.onchange(subject_ids)
+    def on_change_state(self):
+        if len(self.subject_ids) > 3:
+            return {" warning ": {'title':'warning', 'message':'inferireu 3'}}
--- models/etudiant.py
+++ models/etudiant.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 
 class ufec_Etudiant(models.Model):
     _name = 'ufec.etudiant'
-
+    
     image = fields.Binary('')
     nom = fields.Char()
     prenom = fields.Char()
@@ -16,13 +16,19 @@ class ufec_Etudiant(models.Model):
     adreesse = fields.Text()
     date_inscription= fields.Datetime()
 
+    state = fields.Selection([("L1","Level 1"),("L2","Level 2"),("L3","Level 3"),("fini","fini")])
+
     deparment_id = fields.Many2one(comodel_name='ufec.departement')
     classe_id = fields.Many2one(comodel_name='ufec.classe')
+
+    #champs relier
+    subject_ids= fields.Many2many(related='classe_id.subject_ids')
 
     tuteur = fields.Char()
     Non_du_tuteur = fields.Char()
     profession = fields.Char()
     Contacts = fields.Char()
+
 
     @api.multi
     def name_get(self):
@@ -31,3 +37,22 @@ class ufec_Etudiant(models.Model):
             name = '[' + str(etudiant.classe_id.classe_nom) + '] ' + str(etudiant.nom) + ' '+ str(etudiant.prenom)
             result.append((etudiant.id, name))
         return result
+
+
+    @api.one
+    @api.constrains('lieu_de_naissance','date_inscription')
+    def date_check(self):
+        if self.lieu_de_naissance > self.date_inscription:
+            raise ValueError('the binary')
+
+    
+    @api.multi
+    def next_level(self):
+        if self.state == 'L1':
+            return self.write({'state':'L2'})
+        elif self.state == 'L2':
+            return self.write({'state':'L3'})
+        elif self.state == 'L3':
+            return self.write({'state':'fini'})
+        else:
+            return {'warning':{'title': 'fini', 'message':'l elever a deja terminer son annee'}}
